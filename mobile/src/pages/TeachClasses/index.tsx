0b934e4207d587a651f7dd3ac5fda419b5dfd6cc
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-06 18:14:40 -0300

    Added TeachClasses page

--- mobile/src/pages/TeachClasses/index.tsx
+++ mobile/src/pages/TeachClasses/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import teachClassesBgImage from '../../assets/images/teach-classes-background.png';
+
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
 
 function TeachClasses() {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
   return (
-    <View>
-      <Text>hio</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={teachClassesBgImage}
+        resizeMode='contain'
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
     </View>
   );
 }
