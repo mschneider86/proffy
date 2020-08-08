e0bbf2ca78cde7c78380e83210a9d8d342191fa0
Matheus Schneider <matheus.ssm@gmail.com>
2020-08-07 21:07:09 -0300

    added connections route

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -7,6 +7,7 @@ import styles from './styles';
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -27,6 +28,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, isFavorite }) => {
   const [isSavedTeacher, setIsSavedTeacher] = useState(isFavorite);
 
   function handleLinkToWhatsapp() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
   }
 
